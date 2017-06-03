ff5d0659b6247ee65a3968d9fa2124b3d01fba86
AtomicLotus3 <ghs_andre@hotmail.com>
2017-06-03 01:12:40 -0500

    Actualización

--- Adrian/CeramicaCarrillo/Personal/Model/Model1.Designer.cs
+++ Adrian/CeramicaCarrillo/Personal/Model/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Stree\Documents\Visual Studio 2017\Projects\CeramicaCarrillo\Personal\Model\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Proyectos Android\Ceramica_Carrillo\Adrian\CeramicaCarrillo\Personal\Model\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
