699df71681e2cae627cae28d713e09e878d43621
AtomicLotus <ghs_andre@hotmail.com>
2017-08-02 19:37:11 -0500

    Actualización mínima
    Mensajes Venta y apartado

---
+++ Productos/Productos/Model/Mensajes/MensajeVentas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeramicaCarrillo.Model.Mensajes
+{
+    public class MensajeVentaRealizada : Mensaje
+    {
+        public MensajeVentaRealizada(int personal) : base(personal)
+        {
+        }
+
+        public override void IngresarAccion(int personal)
+        {
+            TextoMensaje += " realizó una venta";
+        }
+    }
+
+    public class MensajeApartadoRealizado : Mensaje
+    {
+        public MensajeApartadoRealizado(int personal) : base(personal)
+        {
+        }
+
+        public override void IngresarAccion(int personal)
+        {
+            TextoMensaje += " realizó un apartado";
+        }
+    }
+}
